eae10ebe9582fb9c50242a0ea288180a1dee4faa
Kephren <kephren.delannay.sampany@gmail.com>
2024-02-13 12:49:45 -0500

    ajout des utilisateurs

--- chat/src/client.java
+++ chat/src/client.java
@@ -3,30 +3,52 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
-import java.net.InetAddress;
-import java.net.InetSocketAddress;
 import java.net.Socket;
 
+
+
 public class client {	
+	
     public static void main(String[] args) {
-        try {
+    	try {
         	
         	BufferedReader reader = new BufferedReader(
                     new InputStreamReader(System.in));
+        	
         	System.out.println("Server address : ");
             String serverAddress = reader.readLine();
+            while(!validateIP(serverAddress)) {
+            	System.out.println("Please enter a valid server address : ");
+                serverAddress = reader.readLine();
+            }
+            
             System.out.println("Server port : ");
-        	int serverPort = Integer.parseInt(reader.readLine());
+        	String serverPortInput = reader.readLine();
+        	while(!validatePort(serverPortInput)) {
+            	System.out.println("Please enter a valid port between 5000 and 5050 : ");
+            	serverPortInput = reader.readLine();
+            }
+        	int serverPort = Integer.parseInt(serverPortInput);
+            
         	
-        	InetAddress serverIP = InetAddress.getByName(serverAddress);
-        	
-            // Connect to the server with a specific IP address and port
+        	// Connect to the server with a specific IP address and port
             Socket socket = new Socket(serverAddress, serverPort);
             System.out.println("Connected to server.");
-
-            // Create input and output streams for communication
+            
+         // Create input and output streams for communication
             InputStream input = socket.getInputStream();
             OutputStream output = socket.getOutputStream();
+
+            System.out.println("Username : ");
+            String username = reader.readLine();
+            System.out.println("Password : ");
+            String password = reader.readLine();
+            
+            String accInfos = username + " " + password;
+            
+            output.write(accInfos.getBytes());
+            
+            
 
             // Start a new thread for receiving messages
             Thread receiveThread = new Thread(() -> {
@@ -46,7 +68,9 @@ public class client {
                 }
             });
             receiveThread.start();
-
+            
+            
+            
             // Main thread for sending messages
             while (true) {
                 // Get input from console
@@ -54,8 +78,50 @@ public class client {
                 // Send the message to the server
                 output.write(message.getBytes());
             }
+            
+            // boolean connected = false;
+           
+           
+            
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
+    
+    private static boolean validateIP(String ip) {
+    	if(ip.equals("localhost"))
+    		return true;
+    	
+    	String[] parts = ip.split("\\.");
+        if (parts.length != 4) {
+            return false; // IP address should have exactly 4 parts
+        }
+
+        for (String part : parts) {
+            try {
+                int value = Integer.parseInt(part);
+                if (value < 0 || value > 255) {
+                    return false; // Each part should be in the range [0, 255]
+                }
+            } catch (NumberFormatException e) {
+                return false; // Unable to parse a part as an integer
+            }
+        }
+
+        return true;
+    }
+    
+    private static boolean validatePort(String port) {
+    	try {
+    		int value = Integer.parseInt(port);
+            if (value < 5000 || value > 5050) {
+                return false; // Each part should be in the range [0, 255]
+            }
+        } catch (NumberFormatException e) {
+            return false; // Unable to parse a part as an integer
+        } 
+    	return true;
+    }
+    
 }
+
--- chat/src/server.java
+++ chat/src/server.java
@@ -1,12 +1,17 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.io.Serializable;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
@@ -18,6 +23,8 @@ import java.util.List;
 
 public class server {
 	private static final String HISTORY_FILE = "historique.json";
+	private static final String ACCOUNTS_FILE = "accounts.txt";
+	
 	
     private static List<Socket> clients = new ArrayList<>();
     private static List<Message> history = new ArrayList<>();
@@ -31,8 +38,18 @@ public class server {
                     new InputStreamReader(System.in));
         	System.out.println("Server address : ");
             String serverAddress = reader.readLine();
+            while(!validateIP(serverAddress)) {
+            	System.out.println("Please enter a valid server address : ");
+                serverAddress = reader.readLine();
+            }
+            
             System.out.println("Server port : ");
-        	int serverPort = Integer.parseInt(reader.readLine());
+        	String serverPortInput = reader.readLine();
+        	while(!validatePort(serverPortInput)) {
+            	System.out.println("Please enter a valid port between 5000 and 5050 : ");
+            	serverPortInput = reader.readLine();
+            }
+        	int serverPort = Integer.parseInt(serverPortInput);
         	
             // Create a server socket on a specific port
         	InetAddress serverIP = InetAddress.getByName(serverAddress);
@@ -67,12 +84,35 @@ public class server {
             // Create input and output streams for communication with the client
             InputStream input = clientSocket.getInputStream();
             OutputStream output = clientSocket.getOutputStream();
-            //show 15 first messages in history
-			broadcastHistory(clientSocket);
 
             // Start a new thread for receiving messages from the client
             Thread receiveThread = new Thread(() -> {
-                try {                	
+                try {    
+                	String user;
+                	while (true) {
+                		 byte[] buffer = new byte[1024];
+                         int bytesRead = input.read(buffer);
+                         String receivedCredentials = new String(buffer, 0, bytesRead);
+                         
+                         if(!receivedCredentials.isEmpty())
+                         {
+                        	 String[] credentials = receivedCredentials.split(" ");
+                        	 user = credentials[0];
+                        	 String pass = credentials[1];
+                        	 accountInfos acc = new accountInfos(user, pass);
+                        	 if(validateAccount(acc)) {
+                        		String message = "Connected to the server as " + acc.username;
+                        		clientSocket.getOutputStream().write(message.getBytes());
+                        		//show 15 first messages in history
+                    			broadcastHistory(clientSocket);
+                        		break;
+                        	 } else {
+                        		String message = "Invalid password for user " + acc.username;
+                         		clientSocket.getOutputStream().write(message.getBytes());
+                        	 }
+                         }
+                	}
+                	
                     while (true) {
                         byte[] buffer = new byte[1024];
                         int bytesRead = input.read(buffer);
@@ -80,10 +120,10 @@ public class server {
                             break; // Connection closed by client
                         }
                         String receivedMessage = new String(buffer, 0, bytesRead);
-                        System.out.println("Client: " + receivedMessage);
                         
                         //TODO: ajouter le username du client
-                        Message message = new Message("username", clientSocket.getInetAddress().toString(),clientSocket.getPort() ,receivedMessage);
+                        Message message = new Message(user, clientSocket.getInetAddress().toString(),clientSocket.getPort() ,receivedMessage);
+                        System.out.println(message);
                         addToHistory(message);
                         
 
@@ -112,6 +152,41 @@ public class server {
                 }
             }
         }
+    }
+    
+    private static boolean validateIP(String ip) {
+    	if(ip.equals("localhost"))
+    		return true;
+    	
+    	String[] parts = ip.split("\\.");
+        if (parts.length != 4) {
+            return false; // IP address should have exactly 4 parts
+        }
+
+        for (String part : parts) {
+            try {
+                int value = Integer.parseInt(part);
+                if (value < 0 || value > 255) {
+                    return false; // Each part should be in the range [0, 255]
+                }
+            } catch (NumberFormatException e) {
+                return false; // Unable to parse a part as an integer
+            }
+        }
+
+        return true;
+    }
+    
+    private static boolean validatePort(String port) {
+    	try {
+    		int value = Integer.parseInt(port);
+            if (value < 5000 || value > 5050) {
+                return false; // Each part should be in the range [0, 255]
+            }
+        } catch (NumberFormatException e) {
+            return false; // Unable to parse a part as an integer
+        } 
+    	return true;
     }
     
 	private static void broadcastHistory(Socket client) {
@@ -161,7 +236,7 @@ public class server {
 		}
 	}
     
-    private static class Message implements java.io.Serializable{
+    private static class Message implements Serializable{
         private String username;
         private String userIP;
         private int userPort;
@@ -177,5 +252,71 @@ public class server {
             this.timeSent = Calendar.getInstance(); 
             this.monthSent = timeSent.get(Calendar.MONTH) + 1;
         }
+    }
+    
+    private static void ajouterTexteDansFichier(accountInfos account) {
+        try (FileWriter fileWriter = new FileWriter(ACCOUNTS_FILE, true);
+             BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+             PrintWriter printWriter = new PrintWriter(bufferedWriter)) {
+
+            // Écriture du texte dans le fichier
+            printWriter.println(account.username + " " + account.password);
+            // System.out.println("Le texte a été ajouté avec succès dans le fichier.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    
+    private static List<accountInfos> lireFichier(String nomFichier) {
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(nomFichier))) {
+            // System.out.println("Contenu du fichier :");
+            // Lecture et affichage de chaque ligne du fichier
+            String ligne;
+            List<accountInfos> listeAccount = new ArrayList<>();
+            while ((ligne = bufferedReader.readLine()) != null) {
+                // System.out.println(ligne);
+                String[] accountInfosdata = ligne.split(" ");
+                listeAccount.add(new accountInfos(accountInfosdata[0], accountInfosdata[1]));
+            }
+            return listeAccount;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+    
+    private static boolean validateAccount(accountInfos acc) {
+        List<accountInfos> listeAccount = lireFichier(ACCOUNTS_FILE);
+        if(listeAccount.size() == 0) {
+        	ajouterTexteDansFichier(acc);
+        	return true;
+        } else {
+        	for(accountInfos infos: listeAccount) {
+	        	if(acc.username.equals(infos.username)) {
+	        		if(acc.password.equals(infos.password)) {
+	        			return true;
+	        		} else {
+	        			return false;
+	        		}
+	        	}
+        	}
+        	// new account
+        	ajouterTexteDansFichier(acc);
+        	return true;
+        }
+    }
+
+    
+    private static class accountInfos implements Serializable{
+    	private static final long serialVersionUID = 1L;
+		private String username;
+    	private String password;
+    	
+    	public accountInfos(String _username, String _password){
+    		this.username = _username;
+    		this.password = _password;
+    	}
+    	
     }
 }
